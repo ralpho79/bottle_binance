ca388491cd2a0edeb115ebbf7ee14f6d0eb9d26c
ralpho79 <101933657+ralpho79@users.noreply.github.com>
2023-01-16 13:25:09 +0100

    Update _db.py

--- _db.py
+++ _db.py
@@ -1,3 +1,5 @@
+# run this once to set up the database
+
 import sqlite3
 conn = sqlite3.connect('todo.db') # Warning: This file is created in the current directory
 conn.execute("CREATE TABLE todo (id INTEGER PRIMARY KEY, task char(100) NOT NULL, status bool NOT NULL)")
